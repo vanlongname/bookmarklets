5df7a821e5c40e0a128dc2d4ef96cfd5d57fe0cc
TTheHolyOne <69742571+TTheHolyOne@users.noreply.github.com>
2022-05-03 13:15:47 -0500

    Create clear_cookies.js

---
+++ clear_cookies.js
@@ -0,0 +1,1 @@
+javascript:(function(){document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); }); })();
