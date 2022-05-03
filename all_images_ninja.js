a4c2551cf832e4e988827e4e4abc23423589c723
TTheHolyOne <69742571+TTheHolyOne@users.noreply.github.com>
2022-05-03 11:42:05 -0500

    Makes all images ninja
    Makes all images ninja

---
+++ all_images_ninja.js
@@ -0,0 +1,1 @@
+javascript:(function(){Array.prototype.slice.call(document.querySelectorAll('img')).map(function(el){el.src = '//encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRewzQ0rakhrK_tXzBaOeFTZGM7kURCAPofIP77ToEoE-quiBIG%27;});}())
