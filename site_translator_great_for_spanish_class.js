f76561983089291d441ba8a5cca8ff6064372af2
TTheHolyOne <69742571+TTheHolyOne@users.noreply.github.com>
2022-05-03 11:54:26 -0500

    Create site_translator_great_for_spanish_class.js

---
+++ site_translator_great_for_spanish_class.js
@@ -0,0 +1,1 @@
+javascript:(function(){var a=window.getSelection(),b='';if(a!=''){a='text='+encodeURIComponent(a)}else{a='u='+encodeURIComponent(location.href);b='translate'};window.open('https://translate.google.com/'+b+'?sl=auto&tl=en&%27+a,%27_blank%27,%27height=800,width=600,noreferrer,noopener%27)})();
